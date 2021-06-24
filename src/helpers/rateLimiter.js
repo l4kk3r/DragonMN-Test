183ba4f8c94aa365c49a8d843e79bea464171faf
l4kk3r <66743739+l4kk3r@users.noreply.github.com>
2021-06-24 23:04:24 +0300

    Update rateLimiter.js

--- src/helpers/rateLimiter.js
+++ src/helpers/rateLimiter.js
@@ -2,6 +2,6 @@ const RateLimit = require('koa2-ratelimit').RateLimit;
  
 module.exports = RateLimit.middleware({
     message: 'Слишком много запросов к данному ресурсу',
-    interval: { min: 2 }, 
+    interval: { sec: 30 }, 
     max: 5, 
 });
